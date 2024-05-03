5d0dbfbbb6e52d6001b58febea42d9300604e977
Lucas Brito <lucas@simoa.com>
2024-05-03 14:07:32 -0300

    Adicionei o form post ao PHP, bem como os labels e inputs

--- index.php
+++ index.php
@@ -11,7 +11,23 @@
     //Infos do request
     ?>
     <main>
-        <!-- Aqui vão as infos do HTML -->
+        <h1>Comparação do preço de produtos</h1>
+
+        <form action="" method="post">
+        <label for="preco">Preço do produto 01</label>
+        <input type="number" name="v1" id="v1">
+        <label for="qtd">Quantidade/peso do produto 01</label>
+        <input type="number" name="p1" id="p1">
+
+
+        <label for="preco">Preço do produto 02</label>
+        <input type="number" name="v2" id="v2">
+        <label for="qtd">Quantidade/peso do produto 02</label>
+        <input type="number" name="p2" id="p2">
+
+        <input type="submit" value="Calcular">
+        </form>
+        
     </main>
     
 </body>
