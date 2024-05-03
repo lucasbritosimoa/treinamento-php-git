96901afd7696cc14d119479ea476e2904eb79a7b
Lucas Brito <lucas@simoa.com>
2024-05-03 13:31:50 -0300

    Criei o arquivo readme.md e o index.php para iniciar a codar, e posteriormente pushar no repositório remoto

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste de github</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php 
+    //Infos do request
+    ?>
+    <main>
+        <!-- Aqui vão as infos do HTML -->
+    </main>
     
 </body>
+<section id="resultados">
+<?php 
+
+?>
+</section>
 </html>
